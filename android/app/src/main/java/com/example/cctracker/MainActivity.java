f826b9e18c3b50088d422ec3979adeb31affc4b9
yarshaqa <yarshaqa@gmail.com>
2021-04-04 16:31:22 +0300

    Error with JSON parsing 1

---
+++ android/app/src/main/java/com/example/cctracker/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.cctracker;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- lib/CCList.dart
+++ lib/CCList.dart
@@ -1,60 +1,94 @@
 import 'dart:io';
 
 import 'package:cctracker/CCData.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class CCList extends StatefulWidget{
+class CCList extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return CCListState();
   }
 }
 
-class CCListState extends State<CCList>{
+class CCListState extends State<CCList> {
   List<CCData> data = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Awesome CC Tracker'),
-      ),
-      body: Container(
-        child: ListView(
-            children: _buildList()
-        )
-      ),
-            floatingActionButton: FloatingActionButton(
-    child: Icon(Icons.refresh),
-    onPressed: () => _loadCC()
-    )
-    );
+        appBar: AppBar(
+          title: Text('Awesome CC Tracker'),
+        ),
+        body: Container(child: ListView(children: _buildList())),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.refresh), onPressed: () => _loadCC()));
   }
-  List<Widget> _buildList(){
-    return data.map((CCData f ) => ListTile(
-        title: Text(f.symbol),
-        subtitle: Text(f.name),
-        leading: CircleAvatar(child: Text(f.rank.toString())),
-        trailing: Text('\$${f.price.toString()}')
-    )).toList();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCC();
   }
-  _loadCC() async{
-    final response = await http.get('https://api.coincap.io/v2/assets?limit=10', headers: {HttpHeaders.authorizationHeader: "b85da77f-f1a0-4aa9-82bf-7821cca54275"});
-    if(response.statusCode == 200){
-      print(response.body);
-      var allData = (json.decode(response.body) as Map)['data'] as Map<String, dynamic>;
+
+  Widget alert() {
+    return CupertinoAlertDialog(
+        title: Text("you had long pressedon the ListTile" ),
+        content: Text("yes or not"),
+        actions: [
+          CupertinoDialogAction(
+              child: Text("Yes"),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+          CupertinoDialogAction(child: Text("No"))
+        ]);
+  }
+
+  List<Widget> _buildList() {
+    return data
+        .map((CCData f) => ListTile(
+              title: Text(f.symbol),
+              subtitle: Text(f.name),
+              leading: CircleAvatar(child: Text(f.rank.toString())),
+              trailing: Text('\$${f.price.toStringAsFixed(2)}'),
+              onLongPress: () {
+                showDialog(
+                  context: context,
+                  child: alert(),
+                  barrierDismissible: false,
+                );
+              },
+            ))
+        .toList();
+  }
+
+  _loadCC() async {
+    final response = await http
+        .get('https://api.coincap.io/v2/assets?limit=10', headers: {
+      HttpHeaders.authorizationHeader: "b85da77f-f1a0-4aa9-82bf-7821cca54275"
+    });
+    if (response.statusCode == 200) {
+//      print(response.body);
+      List<dynamic> allData =
+          (json.decode(response.body) as Map<String, dynamic>)["data"];
+      print('all data: $allData');
       var ccDataList = List<CCData>();
-      allData.forEach((String key, dynamic val){
-        var record = CCData(name:val['name'], symbol:val['symbol'], rank:val['rank']);
+      for (var val in allData) {
+        var record = CCData(
+            name: val['name'],
+            symbol: val['symbol'],
+            rank: int.tryParse(val['rank'].toString()),
+            price: double.tryParse(val['priceUsd'].toString()));
+
         ccDataList.add(record);
-      });
+      }
 
       setState(() {
         data = ccDataList;
       });
-      print(data);
     }
-  
-}
+  }
 }
